f756ea463726c76dffcb7bfcf0fa5376b7791c39
vennaretrox <tanselsam@gmail.com>
2026-04-07 10:13:58 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -174,7 +174,7 @@ RunService.Heartbeat:Connect(function()
     if not hum or not root then return end
 
     -- sürekli resetlenmeye karşı
-    hum.WalkSpeed = 25
+    hum.WalkSpeed = 23
 
     -- hareket varsa boost
     if hum.MoveDirection.Magnitude > 0 then
