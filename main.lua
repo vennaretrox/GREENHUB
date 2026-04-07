6f01be5791b08b59334a606f936d3be897ea15ca
vennaretrox <tanselsam@gmail.com>
2026-04-07 14:13:42 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -140,7 +140,7 @@ end)
 
 local speedOn = false
 local NORMAL_SPEED = 16
-local SPEED_MULTIPLIER = 5.3 -- burayı artırabilirsin
+local SPEED_MULTIPLIER = 5 -- burayı artırabilirsin
 
 speedBtn.MouseButton1Click:Connect(function()
     speedOn = not speedOn
